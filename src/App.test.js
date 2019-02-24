ebeba4ef9859be2cd4b1bdc68823a25b54040f7f
Eli <ely.nathan93@gmail.com>
2019-02-24 13:00:29 +0000

    component rendering rests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { shallow, mount, render, configure } from 'enzyme';
+import renderer from 'react-test-renderer';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App'
 import Header from './components/Header'
 import Form from './components/Form'
@@ -7,58 +10,96 @@ import Quote from './components/Quote'
 import Schedule from './components/Schedule'
 import Vehicles from './components/Vehicles'
 
-module.exports = {
-  coveragePathIgnorePatterns: ["/src/index.js"]
-}
+configure({ adapter: new Adapter() });
 
-it('Successfully render header', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Header />, div)
-  ReactDOM.unmountComponentAtNode(div)
+describe('<App />', () => {
+  it('Component renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('matches the snapshot', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 })
 
-it('Successfully render form', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Form />, div)
-  ReactDOM.unmountComponentAtNode(div)
+describe('<Header />', () => {
+  it('Successfully render header', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Header />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('matches the snapshot', () => {
+    const tree = renderer.create(<Header />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 })
 
-it('Successfully render quote', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Quote />, div)
-  ReactDOM.unmountComponentAtNode(div)
+describe('<Form />', () => {
+  it('Successfully render form', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Form />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('matches the snapshot', () => {
+    const tree = renderer.create(<Form />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 })
 
-it('Successfully render schedule', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(
-    <Schedule
+describe('<Quote />', () => {
+  it('Successfully render quote', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Quote />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('matches the snapshot', () => {
+    const tree = renderer.create(<Quote />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+})
+
+describe('<Schedule />', () => {
+  it('Successfully render schedule', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(
+      <Schedule
+        date={new Date()}
+        termLength={1}
+        monthlyMax={150}
+        arrangementFee={88}
+        completionFee={20} />, div
+    )
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('matches the snapshot', () => {
+    const tree = renderer.create(<Schedule
       date={new Date()}
       termLength={1}
       monthlyMax={150}
       arrangementFee={88}
-      completionFee={20} />, div
-  )
-  ReactDOM.unmountComponentAtNode(div)
+      completionFee={20} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 })
 
-it('Gets data length', () => {
-  const div = document.createElement('div')
-  const wrapper = ReactDOM.render(<Vehicles data={[]} monthlyMax={200} />, div)
-  expect(wrapper.props.data.length).toEqual(0);
-  ReactDOM.unmountComponentAtNode(div)
-});
+describe('<Vehicles />', () => {
+  it('Successfully render vehicles', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Vehicles data={[]} monthlyMax={200} />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
 
-it('Successfully render vehicles', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Vehicles data={[]} monthlyMax={200} />, div)
-  ReactDOM.unmountComponentAtNode(div)
-})
-
-it('Full app renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+  it('matches the snapshot', () => {
+    const tree = renderer.create(<Vehicles data={[]} monthlyMax={200} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 })
 
 it('Index finds root element to render app', () => {
